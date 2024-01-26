eeaf2f164b11ea7a795d56e42175d695c7901177
“AkashBalasubramani-Router <akashneelesh@gmail.com>
2024-01-26 16:25:51 +0530

    Revert "Merge branch 'starknet-io:main' into main"
    This reverts commit 575361e8d24a715608f84c2c670313414ba8e26c, reversing
    changes made to 1e98d8c28e0e0c363b30b920d4043b8ab4f234e9.

--- frontend/eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // root: true,
-  // "extends": "next/core-web-vitals",
-  root: true,
-  extends: ["custom"],
-  // extends: ["next"],
-  // extends: ['eslint-config-custom'],
-};
